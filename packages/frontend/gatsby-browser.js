bc3187403f8759b144932d6834ebb041eb98b5da
hwaterke <hwaterke@users.noreply.github.com>
2019-04-26 23:56:59 +0200

    Moved to CRA

--- packages/frontend/gatsby-browser.js
+++
@@ -1,8 +0,0 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
-
-// Having bulma here makes sure it is imported before our styled components in both dev and build
-import 'bulma/css/bulma.css'
--- packages/frontend/src/constants/media.js
+++
@@ -1,3 +0,0 @@
-export const sizes = {
-  tablet: 769,
-}
