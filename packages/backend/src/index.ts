9ad7ce71b89ee8f9195359604a07f904bd7c7e34
hwaterke <hwaterke@users.noreply.github.com>
2019-05-04 18:43:40 +0200

    Reading DHT22

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,4 +1,28 @@
 import './env'
 
+import admin from 'firebase-admin'
+// @ts-ignore
+import sensor from 'node-dht-sensor'
+
 // tslint:disable-next-line:no-console
-console.log('Hello World')
+console.log('Reading sensor data')
+
+// tslint:disable-next-line:no-var-requires
+const serviceAccount = require('../serviceAccountKey.json')
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://green-thumb-a43c6.firebaseio.com',
+})
+
+const db = admin.firestore()
+
+sensor.read(22, 23, (err: any, temperature: number, humidity: number) => {
+  if (!err) {
+    db.collection('sensor-data').add({
+      date: admin.firestore.Timestamp.fromDate(new Date()),
+      humidity,
+      temperature,
+    })
+  }
+})
